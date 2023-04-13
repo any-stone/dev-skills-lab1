d82d245986939d762e2142b3d87e79afdb426da6
Annie Kuxa <kuxa.business@gmail.com>
2023-04-12 23:17:23 -0300

    complete Dev Skills Lab Part 1

---
+++ data/skills-data.js
@@ -0,0 +1,10 @@
+const skills = [
+  {text: 'HTML', done: true, _id: 741},
+  {text: 'CSS', done: true, _id: 852},
+  {text: 'JavaScript', done: true, _id: 963},
+  {text: 'TypeScrip', done: false, _id: 126},
+]
+
+export { 
+	skills
+}
