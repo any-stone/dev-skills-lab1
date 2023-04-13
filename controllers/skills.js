fa08e25b7c996214e1423cb6e200bc72423c9fcc
Annie Kuxa <kuxa.business@gmail.com>
2023-04-12 23:46:34 -0300

    complete dev skills lab2

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,8 +1,16 @@
-import { skills } from '../data/skills-data.js'
+// Import the model that we exported in the Todo.js model file
+import { Skill } from '../models/skill.js'
 
 function index(req, res) {
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => { 
+    res.render('skills/index', {
+      skills: skills,
+    })
+  })
+  .catch(error => { // If there's an error, console.log it and redirect back home!
+    console.log(error)
+    res.redirect('/')
   })
 }
 
